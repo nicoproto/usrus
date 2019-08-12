874524f0f7756e56adc2b47ad8473442339e8ebf
Nico Proto <nicolasproto@gmail.com>
2019-08-12 18:16:29 +0200

    Fixing bugs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependant: :destroy
-  has_many :amenities, through: :item_amenities
+  # has_many :amenities, through: :item_amenities
   has_one :photo, dependant: :destroy
 
   validates :address, :capacity, :price, :description, :name, :lat, :lng, presence: true
