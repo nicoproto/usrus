2bf7bf37ccfcc334497fbd067f35c904a5da3f48
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-13 14:25:06 +0200

    resolved error with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_150707) do
+ActiveRecord::Schema.define(version: 2019_08_13_150709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "amenities", force: :cascade do |t|
-    t.string "attribute"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
