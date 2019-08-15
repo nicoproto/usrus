9ee2cb567798d1df8f207c1dda1febadf7237196
Cihad Saglam <saglam.chd@gmail.com>
2019-08-15 12:09:35 +0200

    finished front end avatar apload part

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,5 +2,8 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  # Remove everything else
+  version :avatar do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
