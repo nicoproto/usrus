2906385dc07f8179ba40d6cdfaf2e09c9496af38
Nico Proto <nicolasproto@gmail.com>
2019-08-16 13:58:37 +0200

    fixed

--- app/javascript/components/price_calc.js
+++ app/javascript/components/price_calc.js
@@ -17,7 +17,9 @@ const priceCalc = () => {
   function datediff(date_from, date_to) {
       // Take the difference between the dates and divide by milliseconds per day.
       // Round to nearest whole number to deal with DST.
-      return Math.round((date_to-date_from)/(1000*60*60*24*365));
+      var result = Math.round((date_to-date_from)/(1000*60*60*24*365))
+
+      return result > 0 ? result : 1;
   }
 
   function calculateTotalPrice(event) {
